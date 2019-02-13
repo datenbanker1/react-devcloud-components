f35c64b3138417afd62c56754518fb76e4b7b0c2
Maximilian Ebert <maximilian@Maximilians-MacBook-Pro.local>
2019-02-13 12:29:09 +0100

    @0.1.3 debuging SubAppbar

--- src/styles/SubAppBar.js
+++ src/styles/SubAppBar.js
@@ -1,8 +1,9 @@
 const style = theme => materialUiTheme => ({
   holder: {
     backgroundColor: "#fff",
-    margin: "-8px",
+    margin: "-8px -8px 0px",
     padding: "8px",
+    zIndex: 100,
     whiteSpace: "nowrap"
   }
 });
