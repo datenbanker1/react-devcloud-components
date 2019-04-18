e16a7f8a83c2fc80d8b092303c91d794328abb46
Maximilian Ebert <maximilian@Maximilians-MacBook-Pro.local>
2019-04-18 14:21:18 +0200

    @0.1.34 Appbar has bottom border, and fixing padding issue

--- src/styles/SubAppBar.js
+++ src/styles/SubAppBar.js
@@ -2,14 +2,15 @@ const style = theme => materialUiTheme => ({
   holder: {
     margin: "-8px -8px 0px",
     padding: "8px",
-    height: "56px"
+    height: "41px"
   },
   fixer: {
     position: "fixed",
     backgroundColor: "#fff",
     margin: "-8px -8px 0px",
     zIndex: 100,
-    whiteSpace: "nowrap"
+    whiteSpace: "nowrap",
+    borderBottom: "1px solid " + theme.palette.background
   },
   childrenHolder: {
     padding: "8px"
