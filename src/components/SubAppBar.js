1b98700627dffcbbd5906e8a88170777e027f1b6
Maximilian Ebert <maximilian@Maximilians-MacBook-Pro.local>
2019-04-20 13:41:22 +0200

    @0.1.40 prevent updating on unmounted components

--- src/components/SubAppBar.js
+++ src/components/SubAppBar.js
@@ -4,6 +4,7 @@ import Theme from "./Theme";
 import defaultStyle from "../styles/SubAppBar";
 
 class SubAppBar extends Component {
+  _isMounted = false;
   constructor(props) {
     super(props);
     this.holder = React.createRef();
@@ -12,19 +13,21 @@ class SubAppBar extends Component {
     };
   }
   componentDidMount() {
+    this._isMounted = true;
     this.updateDimensions();
     window.addEventListener("resize", this.updateDimensions.bind(this));
   }
 
   componentWillUnmount() {
     window.removeEventListener("resize", this.updateDimensions.bind(this));
+    this._isMounted = false;
   }
 
   updateDimensions() {
     let barStyle = { display: "none" };
     if (this.holder.current)
       barStyle = { width: this.holder.current.clientWidth };
-    this.setState({ ...this.state, barStyle });
+    if (this._isMounted) this.setState({ ...this.state, barStyle });
   }
 
   render() {
